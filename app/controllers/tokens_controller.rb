4f6e901985b6a1103cfeef01d99ec9e46698ab47
Volkeee <volkeee1@gmail.com>
2017-11-30 23:32:40 +0200

    - "show": shows user`s last created token - "index": shows all user`s tokens; - "destroy" now checks if token is valid or not and then either makes it invalid or valid according to the request; - "validate": initial commit

--- app/controllers/tokens_controller.rb
+++ app/controllers/tokens_controller.rb
@@ -1,26 +1,46 @@
 class TokensController < ApplicationController
 
+  def show
+    render json: @current_user.tokens.order('created_at').last
+  end
+
+  def index
+    render json: @current_user.tokens
+  end
+
   def create
     if @current_user.tokens.find_by(device_params).nil?
       @current_user.tokens.create(device_params)
       @current_user.save
       render status: 200
     else
+
       render json: Error.conflict, status: 409
     end
   end
 
   def destroy
-    if @current_user.tokens.find_by(device_params).nil?
+    if @current_user.tokens.find_by(token: request.headers['Authorization']).nil?
       render json: Error.not_found, status: 400
     else
-      @token = @current_user.tokens.find_by(device_params).update(validity: false)
-      render status: 200
+      @token = @current_user.tokens.find_by(token: request.headers['Authorization'])
+      if @token['validity']
+        @token['validity'] = false
+        @token.save
+      else
+        @error = Error.conflict
+        @error[:response][:description] = 'Token is already invalid'
+        render json: @error, status: 409
+      end
     end
   end
 
   def destroy_all
     @current_user
+  end
+
+  def validate
+
   end
 
   private
