c8300049c403d159951f8c6c4cc3c5a55823a17c
Volkeee <volkeee1@gmail.com>
2017-11-30 23:35:32 +0200

    - Changed the "create" method to make it easier and cleaner, because it was a total mess; - "destroy" method created to remove all user`s information that is currently in use in the development process (tokens at the moment, nothing else); - added the os_version to device_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  skip_before_action :authenticate, only: [:create]
+  skip_before_action :authenticate_request, only: [:create]
 
   def new
     @user = User.new(user_params)
@@ -9,20 +9,30 @@ class UsersController < ApplicationController
     @group_params = group_params
     @group = Group.find(@group_params[:id])
 
-    @user = @group.users.new(user_params)
-    @user.tokens.new(device_params)
-    if User.exists?(name: @user.name, email: @user.email, origin: @user.origin)
-      @user = User.find_by_name(@user.name)
-      render json: Error.conflict.merge(user: @user),
-             status: 409
+    if User.exists?(name: user_params['name'], email: user_params['email'], origin: user_params['origin'])
+      @user = User.find_by(name: user_params['name'], email: user_params['email'], origin: user_params['origin'])
+      @user.tokens.find_by(device_params).nil? ? @token = @user.tokens.new(device_params) : @token = @user.tokens.find_by(device_params)
+      unless @token.validity
+        @token.validity = true
+      end
+
     else
-      @user.save
-      render json: @user
+      @user = @group.users.new(user_params)
+      @token = @user.tokens.new(device_params)
+
     end
+    @token.save
+    @user.save
+    render json: {user: @user, token: @token}
   end
 
   def update
     @current_user.update(user_params)
+  end
+
+  def destroy
+    @current_user.tokens.destroy_all
+    @current_user.destroy
   end
 
   private
@@ -36,6 +46,6 @@ class UsersController < ApplicationController
   end
 
   def device_params
-    params.require(:device).permit(:device_manufacturer, :device_model)
+    params.require(:device).permit(:device_manufacturer, :device_model, :os_version)
   end
 end
