a7c73ca897a855e3b111fb6d5b4cb94272358770
Volkeee <volkeee1@gmail.com>
2017-11-30 23:37:31 +0200

    - Redirected the root to "groups#index"; - Restricted access to user`s tokens by excepting the methods; - Added next routes:   - post 'user/validate', to: 'tokens#validate'   - get 'user/tokens', to: 'tokens#index'   - get 'user/authenticate', to: 'tokens#show'   - delete 'user/deactivate-device', to: 'tokens#destroy'   - delete 'user', to: 'users#destroy'

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'groups#index'
   post 'authenticate', to: 'authentication#authenticate'
   resources :groups do
     resources :users
@@ -8,12 +9,16 @@ Rails.application.routes.draw do
     resources :tasks
   end
   resources :users do
-    resources :tokens
+    resources :tokens, except: %i[new create destroy index update]
   end
 
   resources :tasks
 
   post 'user/new-device', to: 'tokens#create'
-  post 'user/deactivate-device', to: 'tokens#destroy'
+  post 'user/validate', to: 'tokens#validate'
+  get 'user/tokens', to: 'tokens#index'
+  get 'user/authenticate', to: 'tokens#show'
+  delete 'user/deactivate-device', to: 'tokens#destroy'
+  delete 'user', to: 'users#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
