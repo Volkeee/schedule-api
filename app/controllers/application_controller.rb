95f39e3c12dec7c5cde06a01bfb3aba1c2493af4
Volkeee <volkeee1@gmail.com>
2017-11-30 23:27:19 +0200

    Changed how the authentication works: now it uses token, received in the request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,17 @@
 class ApplicationController < ActionController::API
   include ActionController::HttpAuthentication::Token::ControllerMethods
-  before_action :authenticate
+  before_action :authenticate_request
 
   # attr_reader :current_user
   # helper_method :current_user
 
   private
 
-  #
-  # def authenticate_request
-  #   @current_user = AuthorizeApiRequest.call(request.headers).result
-  #
-  #   render json: Error.unauthorized, status: 401 unless @current_user
-  # end
+  def authenticate_request
+    @current_user = AuthorizeApiRequest.call(request.headers).result
+
+    render json: Error.unauthorized, status: 401 unless @current_user
+  end
 
   def authenticate
     authenticate_or_request_with_http_token do |token, _options|
